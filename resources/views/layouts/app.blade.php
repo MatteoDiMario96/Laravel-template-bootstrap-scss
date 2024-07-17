92a40e7b2154cf5c22a2cc774d929c96b614ce67
MatteoDiMario96 <dm.matteo96@live.it>
2024-07-17 18:26:52 +0200

    Template migliorato. Con layout base pagina home...

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title-page', '')</title>
+    <!--Yeld CDN -->
+    @yield('cdn-yeld')
     <!--Serve a includere il file JS-->
     @vite('resources/js/app.js')
-
-    <title>Document</title>
 </head>
 <body>
-
+    @yield('main-content')
 </body>
 </html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title-page', 'Template Creato!')
+
+@section('main-content')
+    <h1>
+        Template Creato
+    </h1>
+@endsection
